9c6046ab87ebb007ee2adc5364e2e74ca4658ad8
Pumin Swangjang <fvck3oy@hotmail.com>
2019-06-16 19:30:37 +0700

    build

---
+++ build/precache-manifest.c666946d6bc36274f507563a19279cef.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e83f3ecee99386f04c9a0cccb4ff263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd19c23822c71adfce80",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "266de8ae1434397535df",
+    "url": "/static/css/main.15149c75.chunk.css"
+  },
+  {
+    "revision": "dd19c23822c71adfce80",
+    "url": "/static/js/2.67616e74.chunk.js"
+  },
+  {
+    "revision": "266de8ae1434397535df",
+    "url": "/static/js/main.dab6b598.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a6a031d00e3ece0af868c0eef0818a",
+    "url": "/static/media/asparagus_soup.12a6a031.jpg"
+  },
+  {
+    "revision": "51c47f2453f787a4233c630d4feafd6b",
+    "url": "/static/media/bbq_chicken_pizza.51c47f24.jpg"
+  },
+  {
+    "revision": "5f7f32a5296a82665401b169f36e9343",
+    "url": "/static/media/beef_stroganoff.5f7f32a5.jpg"
+  },
+  {
+    "revision": "9a684e92680dff723f8d1b84d5671bfa",
+    "url": "/static/media/caesar_salad.9a684e92.jpg"
+  },
+  {
+    "revision": "720351b37662ad13e6e3d9d6f31cb8c7",
+    "url": "/static/media/carrot_soup.720351b3.jpg"
+  },
+  {
+    "revision": "09bab5538d35bc934dcbeec6e115844b",
+    "url": "/static/media/chicken_cream_soup.09bab553.jpg"
+  },
+  {
+    "revision": "7fea02700c97b82b73e9175a356bcd76",
+    "url": "/static/media/chicken_wonton_soup.7fea0270.jpg"
+  },
+  {
+    "revision": "954a636ed39f0a66375f402a62993fd5",
+    "url": "/static/media/cocktail.954a636e.jpg"
+  },
+  {
+    "revision": "201baef1163015bbac0c740ae239202f",
+    "url": "/static/media/corn_soup.201baef1.jpg"
+  },
+  {
+    "revision": "c2a40fbcef9398702c5a3553f4eaf659",
+    "url": "/static/media/cream_brulee.c2a40fbc.jpg"
+  },
+  {
+    "revision": "dcb5ee6d05f0a2d2db37981d6c2a57d4",
+    "url": "/static/media/fruit_juice.dcb5ee6d.jpg"
+  },
+  {
+    "revision": "9d03b6bf95d26818fa6dc4bf7cff5532",
+    "url": "/static/media/greek_salad.9d03b6bf.jpg"
+  },
+  {
+    "revision": "e9a7d3d0e3ec6b7468e022a8ca050d68",
+    "url": "/static/media/griied_chicken_pesto_sandwiches.e9a7d3d0.jpg"
+  },
+  {
+    "revision": "24d0ebe2fb47f9d9b7ad70f5a975770a",
+    "url": "/static/media/hawaiian_pizza.24d0ebe2.jpg"
+  },
+  {
+    "revision": "abd804f719e5528730f29894c22445a0",
+    "url": "/static/media/local_beer.abd804f7.jpeg"
+  },
+  {
+    "revision": "2eec48c4f70b7184a04e28deca1e026b",
+    "url": "/static/media/mango_cheese_cake.2eec48c4.jpg"
+  },
+  {
+    "revision": "4675f48db323c7f938091e41bd82fc20",
+    "url": "/static/media/margherita.4675f48d.jpg"
+  },
+  {
+    "revision": "996de6b70766fe0a930d892232e17db4",
+    "url": "/static/media/milk_shake.996de6b7.jpg"
+  },
+  {
+    "revision": "924604de31c8d2c5a48eac7ffcd46e8d",
+    "url": "/static/media/mocktail.924604de.jpg"
+  },
+  {
+    "revision": "324089a951763f5416f09e3fbee213f1",
+    "url": "/static/media/potato_soup.324089a9.jpg"
+  },
+  {
+    "revision": "1d2efe8bcba60dd96ff8e46bddae1cfe",
+    "url": "/static/media/prawn_tempura.1d2efe8b.jpg"
+  },
+  {
+    "revision": "bed6d42d00812b3ebfbd0700479f254c",
+    "url": "/static/media/pumpkin_soup .bed6d42d.jpg"
+  },
+  {
+    "revision": "6dda068732e6625703de04f0df0fad4f",
+    "url": "/static/media/smoked_duck_breast_salad.6dda0687.jpg"
+  },
+  {
+    "revision": "b01e9d6e8d354721af6f791ef4c61402",
+    "url": "/static/media/smoked_salman_salad.b01e9d6e.jpg"
+  },
+  {
+    "revision": "bc12d5ebd29310194793c99f564da672",
+    "url": "/static/media/soft_drink.bc12d5eb.jpg"
+  },
+  {
+    "revision": "bbb1b334eaf9e4c8b6d573f9bae12f97",
+    "url": "/static/media/spaghetti_with_baby_squid.bbb1b334.jpg"
+  },
+  {
+    "revision": "4a2356709ace4dafe1ac505e0836ab38",
+    "url": "/static/media/the_vijitt_cheese_burger.4a235670.jpg"
+  },
+  {
+    "revision": "d9ceb784842fc43b99c56839cf5605a8",
+    "url": "/static/media/the_vijitt_club_sandwiches.d9ceb784.jpg"
+  },
+  {
+    "revision": "0e163d6a941a7e70f73e99f94cf52a90",
+    "url": "/static/media/the_vijitt_combination.0e163d6a.jpg"
+  },
+  {
+    "revision": "fac576f4f1e68fd0609e7cbeb14a7b84",
+    "url": "/static/media/tomato_soup.fac576f4.jpg"
+  },
+  {
+    "revision": "3e1b0c4ec518e5432d732a80f8986261",
+    "url": "/static/media/tuna_sashimi.3e1b0c4e.jpg"
+  },
+  {
+    "revision": "aa4ac08413686ff2de1db21683d4bb6b",
+    "url": "/static/media/vegetarian_pizza.aa4ac084.jpg"
+  },
+  {
+    "revision": "ebae87d0d8046d7d096bee2cc14bd74b",
+    "url": "/static/media/vietnamese_fresh_spring_rolls.ebae87d0.jpg"
+  },
+  {
+    "revision": "5e5b32c1dc90858b64d5bc08fadf2a1b",
+    "url": "/static/media/warm_apple_pie.5e5b32c1.jpg"
+  }
+]);
